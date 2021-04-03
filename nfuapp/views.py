806a365fa17d9378ec1a457c04de4fd01e6583ca
gameboy-stack <gt173211@gmail.com>
2021-04-03 09:52:46 +0530

    fllwng

--- nfuapp/views.py
+++ nfuapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from nfu.settings import *
-from . import fllwng
 import os
 def home(request):
 	hell = "Follwng_final.txt"
-	fwng = []
-	fwngobj = fllwng.fllwngcls()
-	fwng = fwngobj.fllwnglistfunc()
-	return render(request,'home.html',{'flwng':fwng})
+	dirf = os.path.join(BASE_DIR,hell)
+	fllwng = []
+
+	with open(dirf) as f:
+		fllwng = [line.rstrip() for line in f]
+
+	chckng = dirf if dirf!="" else "nthng"
+
+	return render(request,'home.html',{'check':chckng,'flwng':fllwng})
