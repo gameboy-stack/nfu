3322b68d8d95dbb3f4f0d753b873d3a54288eb14
gameboy-stack <gt173211@gmail.com>
2021-04-04 15:18:48 +0530

    instabot

--- nfuapp/views.py
+++ nfuapp/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from nfu.settings import *
 from . import fllwng
+from instabot import Bot
 import os
-def home(request):	#,acc,acctype
+def home(request):	#,acc
 	hell = "Follwng_final.txt"
 	fwng = []
 	fwngobj = fllwng.fllwngcls()
-	fwng = fwngobj.fllwnglistfunc()
-	profile = fwngobj.logi("_._.venki._._")
+	fwng = fwngobj.fllwnglistfunc() # pre prepared following list
+	profile = fwngobj.logi("_._.venki._._") # acc 
+		"""	if(profile.is_private):
+		if(profile.followed_by_viewer == False):"""
+
 	print("prof priv")
 	print(profile.is_private)
 	print("follow by view")
@@ -16,5 +20,8 @@ def home(request):	#,acc,acctype
 
 	ntfufinallist = list(set(usrnfu) - set(fwng))
 	ntfufinallist.sort()
+	bot = Bot()
+	bot.login(username="ntf_u",password="=-0987654321`")
+	bot.follow("nibba_12_234")
 
 	return render(request,'home.html',{'no':len(ntfufinallist),'flwng':ntfufinallist})
